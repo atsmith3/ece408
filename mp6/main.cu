2ab0535ec5f3d2e260969665b0b6a973f0d117b1
Andrew <atsmith3@illinois.edu>
2019-11-05 13:43:26 -0600

    convert to uint and convert to float working

--- mp6/main.cu
+++ mp6/main.cu
@@ -15,28 +15,39 @@
     }                                                                     \
   } while (0)
 
-__global__ void cast_float_to_uint8(float* input, uint8_t* output, int width, int height, int channels) {
+
+__global__ void f2u(float* input, uint8_t* output, int width, int height, int channels) {
+  int pos = blockIdx.x*blockDim.x + threadIdx.x;
+  if(pos < width*height*channels) {
+    output[pos] = (uint8_t)(255*input[pos]);
+  }
+}
+
+__global__ void u2f(uint8_t* input, float* output, int width, int height, int channels) {
+  int pos = blockIdx.x*blockDim.x + threadIdx.x;
+  if(pos < width*height*channels) {
+    output[pos] = (float)(input[pos]/255.0);
+  }
+}
+
+__global__ void conv_gs(uint8_t* input, uint8_t* gs, int width, int height, int channels) {
 
 
 }
 
-__global__ void cast_uint8_to_float(uint8_t* input, float* output, int width, int height, int channels) {
-
-
-}
-
-__global__ void convert_to_greyscale(uint8_t* input, uint8_t* gs, int width, int height, int channels) {
-
-
-}
-
-__global__ void histogram(uint8_t* gs, uint32_t* hist) {
+__global__ void histogram(uint8_t* gs, uint32_t* hist, int height, int width, int len) {
 
 
 }
 
 __global__ void scan(uint32_t* hist, uint32_t* cdf, int len) {
 
+
+}
+
+__global__ void equalize(uint32_t* hist, int len, uint8_t* image, int height, int width, int channels) {
+
+//  return (uint8_t)min(max((255*(cdf[val] - cdfmin)/(1.0 - cdfmini)), 0), 255.0);
 
 }
 
@@ -58,12 +69,41 @@ int main(int argc, char **argv) {
   inputImageFile = wbArg_getInputFile(args, 0);
   
   inputImage = wbImport(inputImageFile);
+  hostInputImageData = wbImage_getData(inputImage);
   imageWidth = wbImage_getWidth(inputImage);
   imageHeight = wbImage_getHeight(inputImage);
   imageChannels = wbImage_getChannels(inputImage);
   outputImage = wbImage_new(imageWidth, imageHeight, imageChannels);
+  hostOutputImageData = wbImage_getData(outputImage);
 
-  //@@ insert code here
+  float* deviceInputImage;
+  float* deviceOutputImage;
+  uint8_t* deviceUcharImage;
+  uint8_t* deviceGSImage;
+
+  _check(cudaMalloc((void**)&deviceInputImage, imageWidth*imageHeight*imageChannels*sizeof(float)));
+  _check(cudaMalloc((void**)&deviceOutputImage, imageWidth*imageHeight*imageChannels*sizeof(float)));
+  _check(cudaMalloc((void**)&deviceUcharImage, imageWidth*imageHeight*imageChannels*sizeof(uint8_t)));
+  _check(cudaMalloc((void**)&deviceGSImage, imageWidth*imageHeight*sizeof(uint8_t)));
+
+  _check(cudaMemcpy(deviceInputImage, hostInputImageData, imageWidth*imageHeight*imageChannels*sizeof(float), cudaMemcpyHostToDevice));
+
+  dim3 grid;
+  dim3 block;
+
+  // Convert float to uint8_t
+  grid = dim3(ceil((float)(imageWidth*imageHeight*imageChannels)/512), 1, 1);
+  block = dim3(512, 1, 1);
+  f2u<<<grid, block>>>(deviceInputImage, deviceUcharImage, imageWidth, imageHeight, imageChannels);
+  cudaDeviceSynchronize();
+
+  // Convert uint8_t to float
+  grid = dim3(ceil((float)(imageWidth*imageHeight*imageChannels)/512), 1, 1);
+  block = dim3(512, 1, 1);
+  u2f<<<grid, block>>>(deviceUcharImage, deviceOutputImage, imageWidth, imageHeight, imageChannels);
+  cudaDeviceSynchronize();
+
+  _check(cudaMemcpy(hostOutputImageData, deviceOutputImage, imageWidth*imageHeight*imageChannels*sizeof(float), cudaMemcpyDeviceToHost));
 
   wbSolution(args, outputImage);
 
--- mp6/run.sh
+++ mp6/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cuda-gdb --args ./histogram -i ./data/$1/input.ppm -o ./data/$1/run.ppm -t image -e ./data/$1/output.ppm
+./histogram -i ./data/$1/input.ppm -o ./data/$1/run.ppm -t image -e ./data/$1/output.ppm
+#cuda-gdb --args ./histogram -i ./data/$1/input.ppm -o ./data/$1/run.ppm -t image -e ./data/$1/output.ppm
