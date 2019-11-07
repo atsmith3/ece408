2dfea8d74615bfee9593314a2400ecf931452e66
Andrew <atsmith3@illinois.edu>
2019-11-06 22:16:05 -0600

    Run Script displays with imagemagick

--- mp6/run.sh
+++ mp6/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 ./histogram -i ./data/$1/input.ppm -o ./data/$1/run.ppm -t image -e ./data/$1/output.ppm
+display data/$1/input.ppm &
+display data/$1/run.ppm &
+display data/$1/output.ppm &
 #cuda-gdb --args ./histogram -i ./data/$1/input.ppm -o ./data/$1/run.ppm -t image -e ./data/$1/output.ppm
